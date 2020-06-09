fd6c0f7d7720cd2670edb511a5cc718f83b6efdd
steveneighbour <steve.neighbour@gmail.com>
2020-06-09 12:14:59 +1000

    Update speedtest.sh

--- speedtest.sh
+++ speedtest.sh
@@ -5,13 +5,13 @@ hostname=$(hostname)
 
 echo "Current timestamp: $timestamp"
 
-output=$(speedtest-cli --simple --bytes --server $SERVER)
+output=$(speedtest-cli --simple)
 
 #https://c.speedtest.net/speedtest-servers-static.php
 
 echo "Output: $output"
 
-line=$(echo -n "$output" | awk '/Ping/ {print "ping=" $2} /Download/ {print "download=" $2 * 1000 * 1000} /Upload/ {print "upload=" $2 * 1000 * 1000}' | tr '\n' ',' | head -c -1)
-curl -XPOST "http://$INFLUXDB_HOST:$INFLUXDB_PORT/write?db=$INFLUXDB_DATABASE" -d "speedtest,host=$hostname$EXTRA_TAGS $line $timestamp"
+line=$(echo -n "$output" | awk '/Ping/ {print "ping=" $2} /Download/ {print "download=" $2} /Upload/ {print "upload=" $2}' | tr '\n' ',' | head -c -1)
+curl -XPOST "http://$INFLUXDB_HOST:$INFLUXDB_PORT/write?db=$INFLUXDB_DATABASE" -d "speedtest,host=$hostname $line $timestamp"
 
 echo "New speedtest sent"
